53c028d133417a17a1c571e804a1b13e18083c6b
StArBoY-Works <ameyaworks2001@gmail.com>
2023-05-13 20:51:27 +0530

    Modified Dinning Philosopher in C

--- diningPhilosopher.c
+++ diningPhilosopher.c
@@ -97,108 +97,111 @@ int main()
 
 void * Philosopher(void * philo_num)
 {
-	// First converting the number passed as a void * into integer.
-	int philo = * (int *) philo_num;
+	while(1)
+	{
+		// First converting the number passed as a void * into integer.
+		int philo = * (int *) philo_num;
 
-	/* 
-	
-	 # Then calling the sem_wait function which first checks if the resource is available and if it is available, the resource is allocated to the philosopher i.e. the semaphore is locked.
-	 
-	 # At the backend the sem_wait function for counting semaphore looks like :-
-	  >--------------------------------------------------------------------<
-	  	struct semaphore 
-		{
-      			int count;
-      			queueType queue;
-		};
-		void sem_wait(semaphore s)
-		{		
-      			s.count--;
-      			if(s.count<0)
-      			{
-            			// place this process in s.queue
-            			// block this process     
-      			}
-		}
-	   >-------------------------------------------------------------------<
-
-	 # So here, the number of semaphores is decremented, meaning one of the semaphores is allocated. If all of the resources are allocated, the thread is placed on waiting queue.
-
-	*/ 
-	sem_wait(&room);
-
-	printf("\nPhilosopher %d has Entered the Room", philo);
-	
-	/*
-	
-	 # Now, we apply sem_wait on forks which are binary semaphores.
-
-	 # At the backend the sem_wait function for binary semaphore looks like :-
-	  >---------------------------------------------------------------------<
-		struct binary_semaphore 
-		{
-      			enum {zero,one} value;
-      			queueType queue;
-		};	
-
-		void sem_wait(binary_semaphore s)
-		{
-      			if(s.value == one)
-           		{
-				s.value = zero;
-			}
-      			else
-      			{
-              			// place this process in s.queue 
-              			// block this process
-      			}
-		}
-	   >--------------------------------------------------------------------<
-
-	 # Here, according to the prototype, if the value of semaphore is one, it is changed to zero indicating that the semaphore is blocked.
-
-	 # In our case we are blocking the forks towards the left and the right of the philosopher.
-	 
-	 # For example, for philosopher P0, we are blocking Fork F0 and F4. 	   
-
-	*/ 
-	sem_wait(&Fork[philo]);
-	sem_wait(&Fork[(philo + 1) % 5]);
-
-	// Then, we are allowing the philosophers to eat.
-	Eat(philo);
-
-	sleep(2);
-
-	printf("\nPhilosopher %d has Finished Eating", philo);
-	
-	/*
-	 # Finally, we are freeing the semaphores by calling the sem_post() function so that the other threads that are placed on the queue can use the resources.
-
-	 # At the backend the sem_post function for binary semaphore looks like :-
-	  >----------------------------------------------------------------------<
-	  	void sem_post(semaphore s)
-		{
-      			if(s.queue is empty)
+		/* 
+		
+		# Then calling the sem_wait function which first checks if the resource is available and if it is available, the resource is allocated to the philosopher i.e. the semaphore is locked.
+		
+		# At the backend the sem_wait function for counting semaphore looks like :-
+		>--------------------------------------------------------------------<
+			struct semaphore 
 			{
-           		 	s.value=one;
+					int count;
+					queueType queue;
+			};
+			void sem_wait(semaphore s)
+			{		
+					s.count--;
+					if(s.count<0)
+					{
+							// place this process in s.queue
+							// block this process     
+					}
 			}
-      			else
-      			{
-            			// remove a process P from s.queue 
-            			// place process P on ready list 
-      			}
-		}
-	  >----------------------------------------------------------------------<
-	  
-	 # If it returns a positive value, the semaphore is unlocked successfully. 
+		>-------------------------------------------------------------------<
 
-	 # Using the above functions, we free all the semaphores so that they can be used by other threads.
-	 
-	 */	
-	sem_post(&Fork[(philo + 1) % 5]);
-	sem_post(&Fork[philo]);
-	sem_post(&room);
+		# So here, the number of semaphores is decremented, meaning one of the semaphores is allocated. If all of the resources are allocated, the thread is placed on waiting queue.
+
+		*/ 
+		sem_wait(&room);
+
+		printf("\nPhilosopher %d has Entered the Room", philo);
+		
+		/*
+		
+		# Now, we apply sem_wait on forks which are binary semaphores.
+
+		# At the backend the sem_wait function for binary semaphore looks like :-
+		>---------------------------------------------------------------------<
+			struct binary_semaphore 
+			{
+					enum {zero,one} value;
+					queueType queue;
+			};	
+
+			void sem_wait(binary_semaphore s)
+			{
+					if(s.value == one)
+					{
+					s.value = zero;
+				}
+					else
+					{
+							// place this process in s.queue 
+							// block this process
+					}
+			}
+		>--------------------------------------------------------------------<
+
+		# Here, according to the prototype, if the value of semaphore is one, it is changed to zero indicating that the semaphore is blocked.
+
+		# In our case we are blocking the forks towards the left and the right of the philosopher.
+		
+		# For example, for philosopher P0, we are blocking Fork F0 and F4. 	   
+
+		*/ 
+		sem_wait(&Fork[philo]);
+		sem_wait(&Fork[(philo + 1) % 5]);
+
+		// Then, we are allowing the philosophers to eat.
+		Eat(philo);
+
+		sleep(2);
+
+		printf("\nPhilosopher %d has Finished Eating", philo);
+		
+		/*
+		# Finally, we are freeing the semaphores by calling the sem_post() function so that the other threads that are placed on the queue can use the resources.
+
+		# At the backend the sem_post function for binary semaphore looks like :-
+		>----------------------------------------------------------------------<
+			void sem_post(semaphore s)
+			{
+					if(s.queue is empty)
+				{
+						s.value=one;
+				}
+					else
+					{
+							// remove a process P from s.queue 
+							// place process P on ready list 
+					}
+			}
+		>----------------------------------------------------------------------<
+		
+		# If it returns a positive value, the semaphore is unlocked successfully. 
+
+		# Using the above functions, we free all the semaphores so that they can be used by other threads.
+		
+		*/	
+		sem_post(&Fork[(philo + 1) % 5]);
+		sem_post(&Fork[philo]);
+		sem_post(&room);
+	}
 }
 
 void Eat(int philo)
